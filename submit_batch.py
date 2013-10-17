45414a6e4099c4e7c8368d800896c3ef6b0c1112
Jinzhong Zhang <zhangjin@cern.ch>
2013-10-18 01:00:30 +0200

    batch jobs

--- submit_batch.py
+++ submit_batch.py
@@ -30,19 +30,19 @@ cfg_file="dimuon_ratio.cfg"
 #channel = 'dilepton'
 channel = 'dimuon'
 #channel = 'dielectron'
-#mode = 'observed'
-mode = 'expected'
+mode = 'observed'
+#mode = 'expected'
 #mode = 'mass_cteq'
 #mode = 'mass_graviton'
 
-mass_min  =    200.0
+mass_min  =    1500.0
 mass_max  =    1500.1
 mass_inc  =     25.0
 n_iter    =    10000
 n_burn_in =      500
 
-exp_ntoys_per_job = 100           # these params are now used for observed as well
-exp_ntoys_per_mass_point = 2000
+exp_ntoys_per_job = 10           # these params are now used for observed as well
+exp_ntoys_per_mass_point = 10
 
 #
 #------------------------------------------------------------------------
@@ -71,13 +71,12 @@ while _peak < mass_max:
             continue
         os.system('cp run_limit.C '+TMPDIR+';cp dimuon_C.so '+TMPDIR+';cp ws_dimuon_ratio.root '+TMPDIR )
         SHFILE="#!/bin/bash\n"+          \
-            "cd "+_dir+"\n"+\
-            "cd ../../\n"+\
-            "eval `scramv1 runtime -sh`;cd exost\n"+\
-            "source setup/cmssw_setup.sh\n"+\
+            "cd "+_dir+"/../../../\n"+\
+            "eval `scramv1 runtime -sh`\n"+\
+            "source exost/setup/cmssw_setup.sh\n"+\
             "cd "+_dir+"\n"+\
             "cd "+TMPDIR+"\n"
-        SHFILE+='root -l -b -q -n run_limit.C\\(' + \
+        SHFILE+='root -l -b -q -n rootlogon.C run_limit.C\\(' + \
             '\\"'+channel+'\\",' + \
             '\\"'+mode+'\\",' + \
             str(_peak)+',' + \
@@ -95,8 +94,10 @@ while _peak < mass_max:
         os.system("chmod +x "+SHNAME)
         if not simulate:
             if JID==0 and _peak == mass_min:
-                os.system("bsub -q 2nd "+SHNAME)
+#                os.system("bsub -q 2nd "+SHNAME)
                 print SHNAME," has output (LSFxxxxxx)"
+                os.system("./"+SHNAME)
             else:
-                os.system("bsub -o /dev/null -e /dev/null -q 2nd "+SHNAME)#Here I turned off the email notification coming from LSF
+#                os.system("bsub -o /dev/null -e /dev/null -q 2nd "+SHNAME)#Here I turned off the email notification coming from LSF
+	         os.system("./"+SHNAME)
     _peak += mass_inc
