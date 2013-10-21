0c089a40e3c10ff0a75e5041bf28b7d2e0bf54f1
Jinzhong Zhang <zhangjin@cern.ch>
2013-10-20 20:54:48 -0400

    Har

--- HarvestJobs.sh
+++ HarvestJobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mode=expected
-for i in `seq 200 25 3500`
+for i in `seq 200 25 1500`
 do
-   ./merge_ascii.py -f dimuon_ratio_mcmc_limit_m${i}.ascii dimuon_ratio_mcmc_limit_m${i}.[0-9]*.ascii
-  rm dimuon_ratio_mcmc_limit_m${i}.[0-9]*_ascii
+   ./merge_ascii.py -f dimuon_ratio_mcmc_limit_m${i}.ascii dimuon_ratio_mcmc_limit_m${i}.[0-9]_*.ascii
+  rm dimuon_ratio_mcmc_limit_m${i}.[0-9]_*.ascii
 done
 ./merge_ascii.py -f dimuon_ratio_${1}.ascii dimuon_ratio_mcmc_limit_m*.ascii
 rm dimuon_ratio_mcmc_limit_m*.ascii
