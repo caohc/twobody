021d15ca5facea9049886750e3c6fd1c1f73fbb3
Jinzhong Zhang <zhangjin@cern.ch>
2013-11-11 21:52:39 +0100

    harvest

--- HarvestJobs.sh
+++ HarvestJobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #mode=expected
 mode=observed
-for i in `seq 200 25 1500`
+for i in `seq 200 25 3500`
 do
-  if ls dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}.ascii &> /dev/null; then
-     ./merge_ascii.py -f dimuon_ratio_mcmc_limit_m${i}_${mode}.ascii dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}.ascii
-     rm dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}.ascii
+  if ls dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}*.ascii &> /dev/null; then
+     ./merge_ascii.py -f dimuon_ratio_mcmc_limit_m${i}_${mode}.ascii dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}*.ascii
+     rm dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}*.ascii
   else
     echo "dimuon_ratio_mcmc_limit_m${i} do not exist"
   fi
