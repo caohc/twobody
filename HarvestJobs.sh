8ef0442032e3bc91c673a200834e1bf40c26b1b3
Jinzhong Zhang <zhangjin@cern.ch>
2013-11-13 21:50:45 +0100

    dummy

--- HarvestJobs.sh
+++ HarvestJobs.sh
@@ -7,7 +7,7 @@ do
      ./merge_ascii.py -f dimuon_ratio_mcmc_limit_m${i}_${mode}.ascii dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}*.ascii
      rm dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}*.ascii
   else
-    echo "dimuon_ratio_mcmc_limit_m${i} do not exist"
+    echo "dimuon_ratio_mcmc_limit_m${i}.[0-9]*_${mode}* do not exist"
   fi
 done
 ./merge_ascii.py -f dimuon_ratio_${mode}_${1}.ascii dimuon_ratio_mcmc_limit_m*_${mode}.ascii
